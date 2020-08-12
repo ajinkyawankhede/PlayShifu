fd631ff63f687b0a4472c840d474773ee22f765f
Ajinkya Wankhede <ajinkyawankhede@outlook.com>
2020-08-12 21:44:21 +0530

    Server setup

---
+++ server/config/local.env.js
@@ -0,0 +1,10 @@
+module.exports = {
+  secrets: {
+    session: "Playshifu",
+    expiresIn: 2629746000,
+  },
+
+  db: {
+    URI: "mongodb://localhost:27017/Playshifu",
+  },
+};
---
+++ server/route.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var router = express.Router();
+const app = express();
+// const Authenticate = require("./middleware/index");
+
+module.exports = router;
